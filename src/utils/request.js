b978811257484fc82fa7c6f302df068612503480
xudongye <simonye@aliyun.com>
2020-03-16 18:15:05 +0800

    环境配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import {getToken} from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
   // baseURL: process.env.BASE_API, // api的base_url
-  baseURL: '/', // api的base_url
+  baseURL: '/learn', // api的base_url
   timeout: 15000, // 请求超时时间
   withCredentials: true
 })
